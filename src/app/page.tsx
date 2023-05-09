e8dd833d3df28d2b21a823d58afc149bcb24a63b
jakubmanczak <jakub@manczak.net>
2023-05-09 12:11:20 +0200

    lang picker & main page recreate finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 "use client";
 import { IconChevronDown } from "@/components/icons/ChevronDown";
-import { IconClose } from "@/components/icons/Close";
-import { IconSettings } from "@/components/icons/Settings";
 import { useLang } from "@/lib/useLang";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function PageIndex() {
-  const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
+  const [langsOpen, setLangsOpen] = useState<boolean>(false);
   const debateTools = useLang("debateTools");
   const madeWithLove = useLang("madeWithLove");
   const debateUtility = useLang("oxfDebateUtility");
   const ladderGen = useLang("ladderGen");
-  const settings = useLang("settings");
   return (
     <>
       <div className="text-center py-12">
@@ -27,6 +24,25 @@ export default function PageIndex() {
       </div>
 
       <button
+        className={`langbtn ${langsOpen ? "opened" : ""}`}
+        onClick={() => {
+          setLangsOpen(!langsOpen);
+        }}
+      >
+        English
+        <IconChevronDown />
+      </button>
+      <div className={`langpane ${langsOpen ? "" : "hide"}`}>
+        <button>English</button>
+        <button>Polski</button>
+      </div>
+
+      {/* <div className={`langpane ${langsOpen ? "" : "hidden"}`}>
+        <button>English</button>
+        <button>Polski</button>
+      </div> */}
+
+      {/* <button
         className="settingsbtn"
         onClick={() => {
           setSettingsOpen(!settingsOpen);
@@ -35,16 +51,21 @@ export default function PageIndex() {
         {!settingsOpen ? <IconSettings /> : <IconClose />}
       </button>
       <div className={`settingspane ${settingsOpen ? "" : "hidden"}`}>
-        <h2>{settings}</h2>
-        {/* <button>
-          <p>theme</p>
-          <IconChevronDown />
+        <h1>{settings}</h1>
+        <h2>theme</h2>
+        <button>
+          <p>system theme</p>
+          <IconCheckSquare />
         </button>
         <button>
-          <p>language: </p>
-          <IconChevronDown />
-        </button> */}
-      </div>
+          <p>dark theme </p>
+          <IconSquare />
+        </button>
+        <button>
+          <p>light theme</p>
+          <IconSquare />
+        </button> 
+      </div>*/}
     </>
   );
 }

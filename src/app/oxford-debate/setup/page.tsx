0527c24f19a0d115c3c72c4752370205b5614ed9
jakubmanczak <jakub@manczak.net>
2024-02-04 13:58:56 +0100

    componentise string fields in debate conf, add buttons to conf ui

--- src/app/oxford-debate/setup/page.tsx
+++ src/app/oxford-debate/setup/page.tsx
@@ -1,13 +1,11 @@
 "use client";
+import { DebateConfStringsPanel } from "@/components/DebateConfStringsPanel";
+import { LinkButton } from "@/components/LinkButton";
+import { IconList } from "@/components/icons/List";
+import { IconPlayCircle } from "@/components/icons/PlayCircle";
 import { DebateContext } from "@/contexts/DebateContext";
 import { useLang } from "@/lib/useLang";
 import { useContext } from "react";
-
-const inputStyle = `
-  border-2 border-neutral-800 bg-transparent rounded
-  p-2 px-3 outline-none hover:border-neutral-700 focus:border-neutral-600
-  placeholder:text-neutral-700 w-full
-`;
 
 export default function OxfordDebateSetup() {
   const debateContext = useContext(DebateContext);
@@ -17,57 +15,16 @@ export default function OxfordDebateSetup() {
         {useLang("oxfordDebateConfiguration")}
       </h1>
       <p className="text-center text-neutral-500">
-        {useLang("oxfordDebateConfigurationFlavortext")}
+        {debateContext.conf.motion ||
+          useLang("oxfordDebateConfigurationFlavortext")}
       </p>
       <div className="max-w-2xl mx-auto mt-8 flex flex-col gap-4 px-4">
-        <div className="w-full">
-          <div className="flex flex-row justify-between">
-            <p>{useLang("debateMotion")}</p>
-            <p className="text-neutral-500">
-              {useLang("debateMotionFlavortext")}
-            </p>
-          </div>
-          <input
-            type="text"
-            placeholder={useLang("debateMotion")}
-            className={inputStyle}
-          />
+        <DebateConfStringsPanel />
+        <hr className="border-b-2 rounded border-neutral-800 my-2" />
+        <div className="flex flex-row flex-wrap justify-center gap-2">
+          <LinkButton href="/" text="Main Menu" icon={IconList} />
+          <LinkButton href="/" text="Conduct debate" icon={IconPlayCircle} />
         </div>
-        <div className="flex flex-row justify-between gap-4 flex-wrap md:flex-nowrap">
-          <div className="flex flex-col min-w-[48%] w-full">
-            <div className="flex flex-row justify-between">
-              <p>{useLang("proTeam")}</p>
-              <p className="text-neutral-500">{useLang("proTeamFlavortext")}</p>
-            </div>
-            <input
-              type="text"
-              placeholder={useLang("proTeam")}
-              className={inputStyle}
-            />
-          </div>
-          <div className="flex flex-col min-w-[48%] w-full">
-            <div className="flex flex-row justify-between">
-              <p>{useLang("oppTeam")}</p>
-              <p className="text-neutral-500">{useLang("oppTeamFlavortext")}</p>
-            </div>
-            <input
-              type="text"
-              placeholder={useLang("oppTeam")}
-              className={inputStyle}
-            />
-          </div>
-        </div>
-        {/* 
-        
-        
-        */}
-        <p>{debateContext.conf.speechTime}</p>
-        <p>{debateContext.conf.adVocemTime}</p>
-        <p>{debateContext.conf.endProtectedTime}</p>
-        <p>{debateContext.conf.startProtectedTime}</p>
-        <p>{debateContext.conf.beepOnSpeechEnd ? "y" : "n"}</p>
-        <p>{debateContext.conf.beepProtectedTime ? "y" : "n"}</p>
-        <p>{debateContext.conf.visualizeProtectedTimes ? "y" : "n"}</p>
       </div>
     </div>
   );

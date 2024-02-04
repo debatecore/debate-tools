6e86f058fac1fa3e71e6c86f3c67406f7a0edcaf
Mateusz Dobrzynski <mateusz.dobrzynski64@gmail.com>
2024-02-04 16:11:15 +0100

    improve motion generator ui

--- src/app/debate-motion-generator/page.tsx
+++ src/app/debate-motion-generator/page.tsx
@@ -1,21 +1,19 @@
 "use client";
-import { MotionGeneratorButton } from "@/components/MotionGenerator";
+import { MotionGenerator } from "@/components/MotionGenerator";
 import { useLang } from "@/lib/useLang";
 
 export default function debateMotionGenerator() {
-    return (
+  return (
     <div className="flex flex-col min-h-screen">
-        <h1 className="text-3xl mt-8 text-center font-serif">
-          {useLang("debateMotionGenerator")}
-        </h1>
-        <p className="mb-8 text-center text-neutral-500">
-          {useLang("debateMotionGeneratorFlavortext")}
-        </p>
-        <div className="flex flex-col md:flex-row justify-center flex-wrap items-center gap-2 mt-4">
-          <MotionGeneratorButton
-            text={useLang("debateMotionGeneratorRegenerate")}
-          />
-        </div>
+      <h1 className="text-3xl mt-8 text-center font-serif">
+        {useLang("debateMotionGenerator")}
+      </h1>
+      <p className="mb-8 text-center text-neutral-500">
+        {useLang("debateMotionGeneratorFlavortext")}
+      </p>
+      <div className="flex flex-col md:flex-row justify-center flex-wrap items-center gap-2 mt-4">
+        <MotionGenerator text={useLang("debateMotionGeneratorRegenerate")} />
+      </div>
     </div>
-    );
-  }
+  );
+}
--- src/components/MotionGenerator.tsx
+++ src/components/MotionGenerator.tsx
@@ -3,39 +3,56 @@
 import { iconprops } from "@/types/iconprops";
 import { motion } from "@/types/motion";
 import motions from "@/data/motion.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { IconInfo } from "@/components/icons/info";
 
 type debateMotionGeneratorButtonType = {
   text: string;
   icon?: (props: iconprops) => JSX.Element;
-}
-export type { debateMotionGeneratorButtonType }
+};
+export type { debateMotionGeneratorButtonType };
 
-const MotionGeneratorButton = (props: debateMotionGeneratorButtonType) => {
-  const [motion, setMotion] = useState(() => generateMotion());
+const MotionGenerator = (props: debateMotionGeneratorButtonType) => {
+  const [motion, setMotion] = useState<motion | null>(null);
 
   function generateMotion(): motion {
     const motionsCount = motions.length;
     //TO-DO: add option to generate only motions in a chosen language
-    const randomMotion : motion = motions[Math.round(Math.random() * motionsCount)];
+    const randomMotion: motion = motions[Math.floor(Math.random() * motionsCount)];
     return randomMotion;
-  };
+  }
+
+  useEffect(() => {
+    setMotion(() => generateMotion());
+  }, []);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen items-center">
       <button
         onClick={() => setMotion(() => generateMotion())}
-        className="relative p-4 px-24 min-w-[300px] rounded border-2 border-neutral-800 bg-neutral-800 hover:border-neutral-700 overflow-hidden active:border-neutral-500"
+        className="relative p-4 px-24 max-w-[450px] rounded border-2 border-neutral-800
+        bg-neutral-800 hover:border-neutral-700 overflow-hidden active:border-neutral-500"
       >
         <span className="z-20 relative">{props.text}</span>
       </button>
-      <section className="text-center">
-        <p>{motion.motion}</p>
-        <p>{motion.adinfo}</p>
-        <p>{motion.source}</p>
+      <section className="text-center p-10">
+        <p className="text-2xl md:text-2xl">{motion?.motion || ""}</p>
+        <section className="p-5">
+          {motion && motion.adinfo ? (
+            <section className="flex flex-col items-center">
+              <section className="flex max-w-[300px] space-x-1 mt-15">
+                <IconInfo /> <h3>Infoslide</h3>
+              </section>
+              {motion.adinfo}
+            </section>
+          ) : (
+            ""
+          )}
+        </section>
+        <p className="text-center opacity-70">{motion?.source || ""}</p>
       </section>
     </div>
   );
 };
 
-export { MotionGeneratorButton }
+export { MotionGenerator };

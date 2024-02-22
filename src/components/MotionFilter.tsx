149bc41ab435bed19ac61ff5d751d36d297a0fcd
Mateusz Dobrzynski <mateusz.dobrzynski64@gmail.com>
2024-02-22 13:42:29 +0100

    list motion types in motion filter

--- src/components/MotionFilter.tsx
+++ src/components/MotionFilter.tsx
@@ -1,32 +1,37 @@
 import { getSpecificLangString, useLang } from "@/lib/useLang";
 import { langsArray, langsPublicBlacklist } from "@/types/language";
 import { GenericButton } from "./GenericButton";
+import { motionTypes } from "@/types/motion";
 
 const allowedLanguages = [
   ...langsArray
     .filter((lang) => !langsPublicBlacklist.includes(lang))
     .map((lang) => {
-      return {
-        value: getSpecificLangString("selfLanguageString", lang),
-      };
+      return lang;
     }),
 ];
 
 export default function MotionFilter() {
   return (
-    <div className="absolute">
-      <h5>{useLang("motionFilterTitle")}</h5>
+    <div className="absolute bg-zinc-800 p-3">
+      <h5 className="text-center font-bold">{useLang("motionFilterTitle")}</h5>
       <section className="flex flex-col">
-        <h6>{useLang("language")}</h6>
+        <h6 className="text-center">{useLang("language")}</h6>
         {allowedLanguages.map((language) => (
           <label className="flex flex-row justify-between">
-            <span>{language.value}</span>
-            <input type="checkbox" />
+            <span>{getSpecificLangString("selfLanguageString", language)}</span>
+            <input type="checkbox" value={language} />
           </label>
         ))}
       </section>
       <section>
-        <h6>{useLang("motionType")}</h6>
+        <h6 className="text-center">{useLang("motionType")}</h6>
+        {motionTypes.map((motionType) => (
+          <label className="flex flex-row justify-between">
+            <span className="mr-2">{useLang(motionType.motionType)}</span>
+            <input type="checkbox" value={motionType.motionType} />
+          </label>
+        ))}
       </section>
       <GenericButton text={useLang("filterButtonText")} />
     </div>
--- src/types/motion.ts
+++ src/types/motion.ts
@@ -1,10 +1,26 @@
-import { language } from "./language"
+const motionTypes = [
+  { motionType: "motionPlDescriptive", lang: "pl" },
+  { motionType: "motionPlFact", lang: "pl" },
+  { motionType: "motionPlFirstPerson", lang: "pl" },
+  { motionType: "motionPlComparative", lang: "pl" },
+  { motionType: "motionPlDuty", lang: "pl" },
+  { motionType: "motionPlPolicy", lang: "pl" },
+  { motionType: "motionPlValues", lang: "pl" },
+  { motionType: "motionPlEvaluative", lang: "pl" },
+  { motionType: "motionPlRegret", lang: "pl" },
+  { motionType: "motionEnFirstPerson", lang: "en" },
+  { motionType: "motionEnFact", lang: "en" },
+  { motionType: "motionEnPolicy", lang: "en" },
+  { motionType: "motionEnRegret", lang: "en" },
+  { motionType: "motionEnThisHouseSupports", lang: "en" },
+] as const;
+export { motionTypes };
 
 type motion = {
-    lang: string;
-    motion: string;
-    adinfo: string;
-    source: string;
-}
+  lang: string;
+  motion: string;
+  adinfo: string;
+  source: string;
+};
 
-export type { motion }
+export type { motion };

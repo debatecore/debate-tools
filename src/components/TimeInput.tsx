313b656e96631c85ee3706948a6376efa4b72083
Mateusz Dobrzynski <mateusz.dobrzynski64@gmail.com>
2024-05-28 10:57:15 +0200

    [48] swap increment and decrement buttons in time input component

--- src/components/TimeInput.tsx
+++ src/components/TimeInput.tsx
@@ -5,16 +5,6 @@ const TimeInput = (props: { time: number; setTime: (time: number) => void }) => 
     <div className="flex flex-col lg:flex-row lg:gap-4 items-center">
       <div className="flex flex-row">
         <button
-          className="px-3 border-2 hover:bg-neutral-800 border-neutral-800 hover:border-neutral-700 rounded-l"
-          onClick={() => props.setTime(props.time + 60)}
-        >
-          +
-        </button>
-        <div className="border-y-2 border-neutral-800 min-w-32 flex flex-row justify-center p-2">
-          {Math.floor(props.time / 60)}
-          {" minutes"}
-        </div>
-        <button
           className="px-3 border-2 hover:bg-neutral-800 border-neutral-800 hover:border-neutral-700 rounded-r disabled:text-neutral-500 disabled:hover:bg-transparent disabled:hover:border-neutral-800 disabled:cursor-not-allowed"
           disabled={!(props.time - 60 >= 0)}
           onClick={() => {
@@ -23,19 +13,19 @@ const TimeInput = (props: { time: number; setTime: (time: number) => void }) => 
         >
           -
         </button>
-      </div>
-      <p>and</p>
-      <div className="flex flex-row">
+        <div className="border-y-2 border-neutral-800 min-w-32 flex flex-row justify-center p-2">
+          {Math.floor(props.time / 60)}
+          {" minutes"}
+        </div>
         <button
           className="px-3 border-2 hover:bg-neutral-800 border-neutral-800 hover:border-neutral-700 rounded-l"
-          onClick={() => props.setTime(props.time + 15)}
+          onClick={() => props.setTime(props.time + 60)}
         >
           +
         </button>
-        <div className="border-y-2 border-neutral-800 min-w-32 flex flex-row justify-center p-2">
-          {props.time % 60}
-          {" seconds"}
-        </div>
+      </div>
+      <p>and</p>
+      <div className="flex flex-row">
         <button
           className="px-3 border-2 hover:bg-neutral-800 border-neutral-800 hover:border-neutral-700 rounded-r disabled:text-neutral-500 disabled:hover:bg-transparent disabled:hover:border-neutral-800 disabled:cursor-not-allowed"
           disabled={!(props.time - 15 >= 0)}
@@ -44,6 +34,16 @@ const TimeInput = (props: { time: number; setTime: (time: number) => void }) => 
           }}
         >
           -
+        </button>
+        <div className="border-y-2 border-neutral-800 min-w-32 flex flex-row justify-center p-2">
+          {props.time % 60}
+          {" seconds"}
+        </div>
+        <button
+          className="px-3 border-2 hover:bg-neutral-800 border-neutral-800 hover:border-neutral-700 rounded-l"
+          onClick={() => props.setTime(props.time + 15)}
+        >
+          +
         </button>
       </div>
     </div>

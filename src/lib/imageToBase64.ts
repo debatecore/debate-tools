a5d7978d1d1410f7c1fc39aaacbc10bfc000abd8
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2024-06-12 21:09:43 +0200

    [67] fix custom image to base64 conversion for large files

--- src/lib/imageToBase64.ts
+++ src/lib/imageToBase64.ts
@@ -2,10 +2,14 @@ const convertImageToBase64 = (file: File): Promise<string> => {
     return new Promise<string>((resolve) => {
         const fileReader = new FileReader();
         fileReader.onload = function (event) {
-            const arrayBuffer = event.target?.result;
-            const base64Image = btoa(
-                String.fromCharCode(...new Uint8Array(arrayBuffer as ArrayBuffer))
-            );
+            const arrayBuffer = event.target?.result as ArrayBuffer;
+            let binary = ''
+            const bytes = new Uint8Array(arrayBuffer);
+            const length = bytes.length;
+            for (let i = 0; i < length; i++) {
+                binary += String.fromCharCode(bytes[i]);
+            }
+            const base64Image = btoa(binary);
             resolve(base64Image);
         };
         fileReader.onerror = function (error) {
@@ -15,4 +19,4 @@ const convertImageToBase64 = (file: File): Promise<string> => {
     });
 };
 
-export { convertImageToBase64 };
+export { convertImageToBase64 }

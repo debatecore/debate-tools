edeac1336cdeabde3659dd5b9effedf716bc45e4
jakubmanczak <jakub@manczak.net>
2024-06-22 00:07:54 +0200

    pnd quickfix: timeinput mins/secs conjugation

--- src/components/TimeInput.tsx
+++ src/components/TimeInput.tsx
@@ -1,6 +1,26 @@
 "use client";
+import { LangContext } from "@/contexts/LangContext";
+import { useContext } from "react";
 
-const TimeInput = (props: { time: number; setTime: (time: number) => void }) => {
+const TimeInput = (props: {
+  time: number;
+  setTime: (time: number) => void;
+}) => {
+  const lang = useContext(LangContext).lang;
+
+  const minutesPolish = (count: number) => {
+    if (count % 100 >= 12 && count % 100 <= 14) return "minut";
+    if (count % 10 >= 2 && count % 10 <= 4) return "minuty";
+    if (count == 1) return "minuta";
+    return "minut";
+  };
+  const minutesEnglish = (count: number) => {
+    if (count == 1) return "minute";
+    return "minutes";
+  };
+  const secondsPolish = "sekund";
+  const secondsEnglish = "seconds";
+
   return (
     <div className="flex flex-col lg:flex-row lg:gap-4 items-center">
       <div className="flex flex-row">
@@ -15,7 +35,11 @@ const TimeInput = (props: { time: number; setTime: (time: number) => void }) => 
         </button>
         <div className="border-y-2 border-neutral-800 min-w-32 flex flex-row justify-center p-2">
           {Math.floor(props.time / 60)}
-          {" minutes"}
+          {` ${
+            lang === "pl"
+              ? minutesPolish(Math.floor(props.time / 60))
+              : minutesEnglish(Math.floor(props.time / 60))
+          }`}
         </div>
         <button
           className="px-3 border-2 hover:bg-neutral-800 border-neutral-800 hover:border-neutral-700 rounded-r disabled:text-neutral-500 disabled:hover:bg-transparent disabled:hover:border-neutral-800 disabled:cursor-not-allowed"
@@ -37,7 +61,7 @@ const TimeInput = (props: { time: number; setTime: (time: number) => void }) => 
         </button>
         <div className="border-y-2 border-neutral-800 min-w-32 flex flex-row justify-center p-2">
           {props.time % 60}
-          {" seconds"}
+          {` ${lang === "pl" ? secondsPolish : secondsEnglish}`}
         </div>
         <button
           className="px-3 border-2 hover:bg-neutral-800 border-neutral-800 hover:border-neutral-700 rounded-r disabled:text-neutral-500 disabled:hover:bg-transparent disabled:hover:border-neutral-800 disabled:cursor-not-allowed"

dd4eee33e0c4ac09813d6a169178a9bfcee520cc
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2024-08-27 19:45:30 +0200

    [105] implement e2e footer tests

---
+++ tests/e2e/footer-links.test.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+
+test("debatecore homepage", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+  await page.getByRole("link", { name: "debatecore" }).click();
+  await expect(page).toHaveTitle(/.*debatecore.*/);
+  await expect(page).toHaveURL("https://debateco.re");
+});
+
+test("about page", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+  await page.getByRole("link", { name: "About" }).click();
+  await expect(page).toHaveTitle(/.*debatecore.*/);
+  await expect(page).toHaveURL("https://debateco.re/about");
+});
+
+test("license page", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+  await page.getByRole("link", { name: "License (AGPLv3)" }).click();
+  await expect(page).toHaveTitle(/.*debate-tools\/LICENSE.*/);
+  await expect(page).toHaveURL(
+    "https://github.com/debatecore/debate-tools/blob/master/LICENSE"
+  );
+});
+
+test("debate tools repo", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+  await page.getByRole("link", { name: "Source code" }).click();
+  await expect(page).toHaveTitle(/.*debatecore\/debate-tools.*/);
+  await expect(page).toHaveURL(
+    "https://github.com/debatecore/debate-tools/blob/master/LICENSE"
+  );
+});
